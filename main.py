0ea4c163a8c001f66f6944473bad0c23cd41ec56
philippkaufman <phelix.kaufman@gmail.com>
2022-04-04 21:07:53 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from math import sqrt, ceil
+
+def check(n):
+    for i in reversed(range(ceil(sqrt(n))), n):
+        if n/i == 7
+        if n % i == 0:
+            if i % 10 == 7:
+                return i
+    return 0
+
+
+if __name__ == '__main__':
+    i = 50
+    n = 600000
+    while i > 0:
+        n += 1
+        delitel = check(n)
+        if delitel:
+            print(n, delitel)
+            i -= 1
