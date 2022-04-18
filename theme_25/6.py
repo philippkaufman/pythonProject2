2befa98733911c42af5117470c9511b7b2c284df
Ivan Sizov <vsizov63@gmail.com>
2022-04-18 20:33:37 +0300

    18.04

--- theme_25/6.py
+++ theme_25/6.py
@@ -1,16 +1,24 @@
 from math import sqrt, ceil
+
+
 def deliteli(n):
     numdel = 0
     a = 0
-    for i in range(2, ceil(sqrt(n))):
-        if n%i ==0 and i%2 == 0:
-            numdel +=1
-            a = n
+    denomimators = []
+    for i in range(1, ceil(sqrt(n))):
+        if n % i == 0:
+            if i % 2 == 0:
+                numdel += 1
+                denomimators.append(i)
+            if (n // i) % 2 == 0:
+                numdel += 1
+                denomimators.append(n // i)
     if numdel == 4:
-        print(a)
+        print(sorted(denomimators))
 
 
-
-if __name__ =='__main__':
-    for i in range(110203, 110245):
+if __name__ == '__main__':
+    for i in range(110203, 110245 + 1):
         deliteli(i)
+
+
